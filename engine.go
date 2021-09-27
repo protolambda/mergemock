ff59096e88c929f9ac0ee8feeec1b4147a05b75c
lightclient@protonmail.com <lightclient@protonmail.com>
2021-09-28 00:53:45 +0200

    stub in http server with rpc handler to engine

--- engine.go
+++ engine.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"net/http"
 
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/sirupsen/logrus"
 )
 
@@ -13,6 +16,7 @@ type EngineCmd struct {
 	LogCmd `ask:".log"`
 
 	close chan struct{}
+	http  *httpServer
 }
 
 func (c *EngineCmd) Default() {
@@ -31,7 +35,9 @@ func (c *EngineCmd) Run(ctx context.Context, args ...string) error {
 
 	c.close = make(chan struct{})
 
-	// TODO: http server
+	rpcServer := rpc.NewServer()
+	rpcServer.RegisterName("engine", &EngineBackend{})
+	http.Handle("/", &httpServer{rpc: rpcServer})
 
 	go c.RunNode(log)
 
@@ -39,9 +45,8 @@ func (c *EngineCmd) Run(ctx context.Context, args ...string) error {
 }
 
 func (c *EngineCmd) RunNode(log *logrus.Logger) {
-
 	log.Info("started")
-
+	http.ListenAndServe(":8090", nil)
 }
 
 func (c *EngineCmd) Close() error {
@@ -49,4 +54,18 @@ func (c *EngineCmd) Close() error {
 		c.close <- struct{}{}
 	}
 	return nil
+}
+
+type httpServer struct {
+	rpc *rpc.Server
+}
+
+func (h *httpServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.rpc.ServeHTTP(w, r)
+}
+
+type EngineBackend struct{}
+
+func (e *EngineBackend) GetPayload(idx hexutil.Uint64) hexutil.Uint64 {
+	return idx
 }

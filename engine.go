6c08bdff97f81f6d81a7b389cc8090bbae9ccd9a
protolambda <proto@protolambda.com>
2021-09-28 14:31:24 +0200

    update mock engine

--- engine.go
+++ engine.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"log"
+	"net"
 	"net/http"
+	"time"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/rpc"
@@ -15,12 +18,27 @@ type EngineCmd struct {
 	// embed logger options
 	LogCmd `ask:".log"`
 
-	close chan struct{}
-	http  *httpServer
+	ListenAddr string `ask:"--listen-addr" help:"Address to bind RPC HTTP server to"`
+
+	Timeout struct {
+		Read       time.Duration `ask:"--read"`
+		ReadHeader time.Duration `ask:"--read-header"`
+		Write      time.Duration `ask:"--write"`
+		Idle       time.Duration `ask:"--idle"`
+	} `ask:".timeout"`
+
+	close  chan struct{}
+	log    logrus.Ext1FieldLogger
+	ctx    context.Context
+	rpcSrv *rpc.Server
+	srv    *http.Server
 }
 
 func (c *EngineCmd) Default() {
-	// TODO
+	c.Timeout.Read = 30 * time.Second
+	c.Timeout.ReadHeader = 10 * time.Second
+	c.Timeout.Write = 30 * time.Second
+	c.Timeout.Idle = 5 * time.Minute
 }
 
 func (c *EngineCmd) Help() string {
@@ -28,25 +46,50 @@ func (c *EngineCmd) Help() string {
 }
 
 func (c *EngineCmd) Run(ctx context.Context, args ...string) error {
-	log, err := c.LogCmd.Create()
+	logr, err := c.LogCmd.Create()
 	if err != nil {
 		return err
 	}
+	c.log = logr
+	c.ctx = ctx
 
 	c.close = make(chan struct{})
 
-	rpcServer := rpc.NewServer()
-	rpcServer.RegisterName("engine", &EngineBackend{})
-	http.Handle("/", &httpServer{rpc: rpcServer})
+	c.rpcSrv = rpc.NewServer()
+	c.rpcSrv.RegisterName("engine", &EngineBackend{log: logr})
 
-	go c.RunNode(log)
+	mux := http.NewServeMux()
+	mux.Handle("/", c.rpcSrv)
+
+	// log http errors to our logrus logger
+	logw := logr.Writer()
+
+	c.srv = &http.Server{
+		Addr:              c.ListenAddr,
+		Handler:           mux,
+		ReadTimeout:       c.Timeout.Read,
+		ReadHeaderTimeout: c.Timeout.ReadHeader,
+		WriteTimeout:      c.Timeout.Write,
+		IdleTimeout:       c.Timeout.Idle,
+		ConnState: func(conn net.Conn, state http.ConnState) {
+			e := logr.WithField("addr", conn.RemoteAddr().String())
+			e.WithField("state", state.String())
+			e.Debug("client changed state")
+		},
+		ErrorLog: log.New(logw, "", 0),
+		BaseContext: func(listener net.Listener) context.Context {
+			return ctx
+		},
+	}
+
+	go c.RunNode()
 
 	return nil
 }
 
-func (c *EngineCmd) RunNode(log *logrus.Logger) {
-	log.Info("started")
-	http.ListenAndServe(":8090", nil)
+func (c *EngineCmd) RunNode() {
+	c.log.Info("started")
+	c.srv.ListenAndServe()
 }
 
 func (c *EngineCmd) Close() error {
@@ -56,16 +99,12 @@ func (c *EngineCmd) Close() error {
 	return nil
 }
 
-type httpServer struct {
-	rpc *rpc.Server
+type EngineBackend struct {
+	log logrus.Ext1FieldLogger
 }
-
-func (h *httpServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	h.rpc.ServeHTTP(w, r)
-}
-
-type EngineBackend struct{}
 
 func (e *EngineBackend) GetPayload(idx hexutil.Uint64) hexutil.Uint64 {
 	return idx
 }
+
+// TODO: more engine methods

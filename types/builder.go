008b2729827204e53cb32d3ae4148e5bd5ac7952
Chris Hager <chris@linuxuser.at>
2022-05-11 12:46:28 +0200

    fix builder types extra_data

--- types/builder.go
+++ types/builder.go
@@ -96,20 +96,20 @@ type SyncAggregate struct {
 
 // ExecutionPayloadHeader https://github.com/ethereum/consensus-specs/blob/dev/specs/bellatrix/beacon-chain.md#executionpayloadheader
 type ExecutionPayloadHeader struct {
-	ParentHash       Hash    `json:"parent_hash" ssz-size:"32"`
-	FeeRecipient     Address `json:"fee_recipient" ssz-size:"20"`
-	StateRoot        Root    `json:"state_root" ssz-size:"32"`
-	ReceiptsRoot     Root    `json:"receipts_root" ssz-size:"32"`
-	LogsBloom        Bloom   `json:"logs_bloom" ssz-size:"256"`
-	Random           Hash    `json:"prev_randao" ssz-size:"32"`
-	BlockNumber      uint64  `json:"block_number,string"`
-	GasLimit         uint64  `json:"gas_limit,string"`
-	GasUsed          uint64  `json:"gas_used,string"`
-	Timestamp        uint64  `json:"timestamp,string"`
-	ExtraData        Hash    `json:"extra_data" ssz-size:"32"`
-	BaseFeePerGas    U256Str `json:"base_fee_per_gas" ssz-max:"32"`
-	BlockHash        Hash    `json:"block_hash" ssz-size:"32"`
-	TransactionsRoot Root    `json:"transactions_root" ssz-size:"32"`
+	ParentHash       Hash          `json:"parent_hash" ssz-size:"32"`
+	FeeRecipient     Address       `json:"fee_recipient" ssz-size:"20"`
+	StateRoot        Root          `json:"state_root" ssz-size:"32"`
+	ReceiptsRoot     Root          `json:"receipts_root" ssz-size:"32"`
+	LogsBloom        Bloom         `json:"logs_bloom" ssz-size:"256"`
+	Random           Hash          `json:"prev_randao" ssz-size:"32"`
+	BlockNumber      uint64        `json:"block_number,string"`
+	GasLimit         uint64        `json:"gas_limit,string"`
+	GasUsed          uint64        `json:"gas_used,string"`
+	Timestamp        uint64        `json:"timestamp,string"`
+	ExtraData        hexutil.Bytes `json:"extra_data" ssz-size:"32"`
+	BaseFeePerGas    U256Str       `json:"base_fee_per_gas" ssz-max:"32"`
+	BlockHash        Hash          `json:"block_hash" ssz-size:"32"`
+	TransactionsRoot Root          `json:"transactions_root" ssz-size:"32"`
 }
 
 // ExecutionPayload https://github.com/ethereum/consensus-specs/blob/dev/specs/bellatrix/beacon-chain.md#executionpayload
@@ -214,7 +214,7 @@ func PayloadToPayloadHeader(p *ExecutionPayloadV1) (*ExecutionPayloadHeader, err
 		GasLimit:         p.GasLimit,
 		GasUsed:          p.GasUsed,
 		Timestamp:        p.Timestamp,
-		ExtraData:        [32]byte(common.BytesToHash(p.ExtraData)),
+		ExtraData:        hexutil.Bytes(p.ExtraData),
 		BaseFeePerGas:    [32]byte(common.BytesToHash(p.BaseFeePerGas.Bytes())),
 		BlockHash:        [32]byte(p.BlockHash),
 		TransactionsRoot: [32]byte(types.DeriveSha(types.Transactions(txs), trie.NewStackTrie(nil))),
